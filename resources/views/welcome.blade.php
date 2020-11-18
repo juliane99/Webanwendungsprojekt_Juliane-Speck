077dec25fd0592d54d8e6fc1d71e78948bc1c2a4
Juliane Speck <57350721+juliane99@users.noreply.github.com>
2020-11-18 17:35:20 +0100

    chnaged layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
-@extends('diaries.layout')
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Travel Diary</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
@@ -20,7 +20,7 @@
         </style>
     </head>
         <body>
-            
+        @extends('diaries.layout')
         </body>
 </html>
 
