d25e2ba57561d963fa17156309b1a01ca91cd61a
Juliane Speck <57350721+juliane99@users.noreply.github.com>
2020-11-20 14:57:59 +0100

    show blade image tags

--- resources/views/diaries/show.blade.php
+++ resources/views/diaries/show.blade.php
@@ -16,13 +16,9 @@
                <h2> {{ $diary->title }} </h2>
                <p style="font-size:18px;">  {{ $diary->created_at }} </p>
 
-             <!-- <img src="{{ Storage::url($diary->featured_image) }}" height="400" width="800" /> -->
-
-             
-              <!--  <img src="{{ url('/featured_images/'.$diary->featured_image) }}" alt="" title="" /> -->
+            <!-- <img src="{{ Storage::url($diary->featured_image) }}" height="400" width="800" /> -->
 
             <!-- <img src="/storage/featured_images/{{$diary->featured_image}}" height="400" width="800" /> -->
-
 
             </div>
         </div>
